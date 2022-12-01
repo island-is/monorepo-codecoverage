b204f5dd0c53b5b708dc20d7a60613c41ab91d5b
Petar Shomov <pshomov@gmail.com>
2022-12-01 15:45:18 +0000

    moving along

--- nyc.config.js
+++ nyc.config.js
@@ -1,14 +1,29 @@
+const babelConfig = require('@istanbuljs/nyc-config-babel');
+// const hookRunInThisContextConfig = require('@istanbuljs/nyc-config-hook-run-in-this-context');
+const typescript = require("@istanbuljs/nyc-config-typescript");
+const { parserPlugins } = require('@istanbuljs/schema').defaults.nyc;
+
+console.log(`Default plugins: ${parserPlugins}`)
+console.log(`Typescript plugins: ${typescript.parserPlugins}`)
 module.exports = {
-  "extends": "@istanbuljs/nyc-config-typescript",
-  "all": true,
+  ...typescript,
+  ...babelConfig,
+  // "extends": "@istanbuljs/nyc-config-typescript",
+  parserPlugins: parserPlugins.concat('typescript', 'decorators'),
+  // "all": true,
   "clean": true,
-  "reporter": ["html", "text"],
+  // "esModules": true,
+  "reporter": ["html", "text", "json"],
   "include": [
     "**/*.ts",
+    "dist/**/*.js"
   ],
   "exclude": [
-    "**/*.spec.ts"
+    "**/*.spec.ts",
+    "**/jest.config.ts",
+    "**/*.d.ts"
   ],
+  "instrument": true,
   // "skip-full": true,
   // 'excludeNodeModules': false,
   'temp-dir': "./coverage"
