3e46642ead2a01579753c5c2729bc7f6fb09ec3b
Petar Shomov <pshomov@gmail.com>
2023-01-01 21:15:15 +0000

    changing code that should now not decrease code coverage

--- apps/back1/src/app/app.service.ts
+++ apps/back1/src/app/app.service.ts
@@ -5,7 +5,7 @@ import {lib2} from "@my-workspace/lib2"
 @Injectable()
 export class AppService {
   getData(): { message: string, message2: string } {
-    const a = lib1()
+    const a = 'lib1'
     return {message: `Welcome to back1! => ${a}`, message2: lib2()};
   }
 }
