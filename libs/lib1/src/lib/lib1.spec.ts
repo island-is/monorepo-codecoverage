208685f52f6813cdc59ef287129c102787c3188c
Petar Shomov <pshomov@gmail.com>
2023-01-01 21:10:26 +0000

    writing a (bad) test that should not increase component coverage

--- libs/lib1/src/lib/lib1.spec.ts
+++ libs/lib1/src/lib/lib1.spec.ts
@@ -1,7 +1,10 @@
-import {lib1_part2} from './lib1';
+import {lib1, lib1_part2} from './lib1';
 
 describe('lib1', () => {
   it('should work', () => {
     expect(lib1_part2()).toEqual('lib_part21');
+  });
+  it('should work', () => {
+    expect(lib1()).toEqual('lib2');
   });
 });
